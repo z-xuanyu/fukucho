8097afbbfafef966347e544cf317f2cea366f752
z-xuanyu <37470070+z-xuanyu@users.noreply.github.com>
2022-07-15 17:25:22 +0800

    Update vite.config.ts
    更改打包部署路径为根路径

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ function pathResolve(dir: string) {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/fukucho",
+  base: "/",
   resolve: {
     alias: [
       {
